5df55a559cc35380c74585a6021002cada641fb3
TheRandomCrafter83 <williams.carl.83@gmail.com>
2022-05-12 22:13:46 -0400

    test

--- app/src/main/java/com/coderz/f1/activityresultslauncherkotlintextfiles/MainActivity.kt
+++ app/src/main/java/com/coderz/f1/activityresultslauncherkotlintextfiles/MainActivity.kt
@@ -42,7 +42,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    //Test
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
